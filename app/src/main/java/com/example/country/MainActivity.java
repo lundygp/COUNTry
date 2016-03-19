516cdf244cca99eaaf0657fc98c50becfb82c84b
jlmark <jlmark2607@gmail.com>
2016-03-19 20:27:24 +0000

    First Page Done

--- app/src/main/java/com/example/country/MainActivity.java
+++ app/src/main/java/com/example/country/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.country;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        afterFirstClick();
+    }
+
+    public void afterFirstClick()
+    {
+        next = (Button)findViewById(R.id.nextButton);
+        next.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent proceed = new Intent("com.example.country.SelectActivity");
+                        startActivity(proceed);
+                    }
+                }
+        );
     }
 
     @Override
