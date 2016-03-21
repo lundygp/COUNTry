c3c3e4b580de4a11e0d89221622ac73692acea3e
lundygp <lundygeorge@gmail.com>
2016-03-21 18:27:19 +0000

    Added Spinner and toast for Selector Screen + UK

--- app/src/main/java/com/example/country/Select.java
+++ app/src/main/java/com/example/country/Select.java
@@ -1,18 +1,17 @@
 package com.example.country;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class Select extends AppCompatActivity {
 
+    ImageView flagChoiceView;
     Spinner spinnerCountry;
     ArrayAdapter<CharSequence> adapter;
 
@@ -20,6 +19,7 @@ public class Select extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -32,7 +32,71 @@ public class Select extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+" selected", Toast.LENGTH_SHORT).show();
+                flagChoiceView = (ImageView) findViewById(R.id.flagChoice);
+
+               switch(position)
+               {
+                   case 0:
+
+                   flagChoiceView.setImageResource(R.drawable.austriaflag);
+                   break;
+
+                   case 1:
+                       flagChoiceView.setImageResource(R.drawable.belgiumflag);
+                       break;
+
+                   case 2:
+                       flagChoiceView.setImageResource(R.drawable.denmarkflag);
+                       break;
+
+                   case 3:
+                       flagChoiceView.setImageResource(R.drawable.englandflag);
+                       break;
+
+                   case 4:
+                       flagChoiceView.setImageResource(R.drawable.franceflag);
+                       break;
+
+                   case 5:
+                       flagChoiceView.setImageResource(R.drawable.germanflag);
+                       break;
+
+                   case 6:
+                       flagChoiceView.setImageResource(R.drawable.irelandflag);
+                       break;
+
+                   case 7:
+                       flagChoiceView.setImageResource(R.drawable.italyflag);
+                       break;
+
+                   case 8:
+                       flagChoiceView.setImageResource(R.drawable.luxembourgeflag);
+                       break;
+
+                   case 9:
+                       flagChoiceView.setImageResource(R.drawable.portugalflag);
+                       break;
+
+                   case 10:
+                       flagChoiceView.setImageResource(R.drawable.spainflag);
+                       break;
+
+                   case 11:
+                       flagChoiceView.setImageResource(R.drawable.swedenflag);
+                       break;
+
+                   case 12:
+                       flagChoiceView.setImageResource(R.drawable.switzerlandflag);
+                       break;
+
+                   case 13:
+                       flagChoiceView.setImageResource(R.drawable.walesflag);
+                       break;
+
+                   default: flagChoiceView.setImageResource(R.drawable.austriaflag);
+                       break;
+
+               }
 
             }
 
